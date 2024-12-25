99884ec9d320b15578b58c901d06834c22647b66
Airduck <watchurap@gmail.com>
2024-12-25 20:34:03 +0100

    task 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(tArray, rmItem) {
+    return tArray.filter(element => element != rmItem);
 };
 
 // Do not edit below this line
