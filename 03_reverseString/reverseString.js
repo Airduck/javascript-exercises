d86ffa723b11d76f3d21a4aa0c0f28d2d086321d
Airduck <watchurap@gmail.com>
2024-12-25 19:07:09 +0100

    task 4 finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let wordArray = string.split(""); // [w,o,r,d]
+    wordArray = wordArray.reverse();
+    return wordArray.join("")
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
