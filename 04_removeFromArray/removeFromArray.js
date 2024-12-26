585912d1d70a8504e77cfa649dd399de66e4bf95
Airduck <watchurap@gmail.com>
2024-12-26 14:30:50 +0100

    all done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function(tArray, rmItem) {
-    return tArray.filter(element => element != rmItem);
+const removeFromArray = function(tArray, rmItem, rmItem2, rmItem3, rmItem4) {
+    if (!rmItem2 && !rmItem3 && !rmItem4) {
+        return tArray.filter(element => element !== rmItem);
+    } else if (!rmItem3 && !rmItem4) {
+        return tArray.filter(element => element !== rmItem && element !== rmItem2);
+    } else if (!rmItem4) {
+        return tArray.filter(element => element !== rmItem && element !== rmItem2 && element !== rmItem3);
+    } else {
+        return tArray.filter(element => element !== rmItem && element !== rmItem2 && element !== rmItem3 && element !== rmItem4);
+    }
+
 };
 
 // Do not edit below this line
