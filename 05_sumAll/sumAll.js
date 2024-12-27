0c9809890388aeeafc3a2fd456daea18ef2bc1fd
Airduck <watchurap@gmail.com>
2024-12-27 11:34:24 +0100

    all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
+    if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second)) {
+       return "ERROR"; 
 
+    } else if (first < second) {
+        for (let i = first; i <= second; i++) {
+            sum +=  i;
+        }
+        return sum;
+
+    } else if (second < first) {
+        for (let i = second; i <= first; i++) {
+            sum +=  i;
+        }
+        return sum; 
+    } 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
