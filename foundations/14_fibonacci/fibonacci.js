d89b642979b29f213eb4d674e2ae11695f045f5b
Airduck <watchurap@gmail.com>
2026-08-07 12:52:11 +0200

    update

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n===0) {return 0};
+    if (n<0) {return "OOPS"};
+    const arr = [1,1];
+    if (n > 2) {
+        for (let i = 0; i < n-2; i++) {
+            arr.push(arr[i]+arr[i+1]);
+        };
+    };
+    return arr[arr.length -1];
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
