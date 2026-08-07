7f43915088b5980810c5bf99d27f97a22b26f52f
Airduck <watchurap@gmail.com>
2026-08-07 13:30:16 +0200

    update

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (!("yearOfDeath" in arr[i])) {
+            arr[i]["yearOfDeath"] = 2026;
+        };
+    };
+    arr.sort((a,b) => (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth));
+    return arr.pop();
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
